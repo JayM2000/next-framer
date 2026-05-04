db7ef0403118e2121b4d5ca47150bb16164fdc2f
JayM2000 <jaymisal200@gmail.com>
2026-05-04 11:59:19 +0530

    added new url /ping for status of application pinging

---
+++ app/ping/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function PingPage() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'monospace' }}>
+      <h1>Status 200</h1>
+      <p>Service is up and running.</p>
+    </div>
+  );
+}
